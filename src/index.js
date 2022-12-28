9bde3de6b690ec941e90085eff543b5c54c555d4
Miki <mikcat@users.noreply.github.com>
2022-12-28 17:31:17 +0100

    Mini fix import

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './scss/App.scss';
 
 import App from './App';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
